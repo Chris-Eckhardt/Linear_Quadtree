31ce0f35917131559d3d129a44f85bc5cdad3913
Chris Eckhardt <ceckhardt06@gmail.com>
2020-06-15 16:57:11 -0700

    Update window.h

--- window.h
+++ window.h
@@ -4,24 +4,12 @@
 
 #include <stdlib.h>
 
-/****************** BOUND ************/
-
 typedef struct _BOUND {
 	int x;
     int y;
     int width;
     int height;
 } BOUND;
-
-BOUND create_bound(int x, int y, int width, int height);
-
-int bound_contains(BOUND * b, int x, int y);
-
-int bound_intersects(BOUND * a, BOUND * b);
-
-/*********** END BOUND ***************/
-
-/************ QNODE ***************/
 
 typedef struct _QNODE {
 	int has_children;
@@ -33,40 +21,24 @@ typedef struct _QNODE {
 	struct _QNODE * se;
 } QNODE;
 
-QNODE * create_qnode(int x, int y, int width, int height);
+typedef struct _FRAME {
+	BOUND bound;
+	char * title;
+} FRAME;
 
-QNODE * destroy_qnode(QNODE * q);
+typedef struct _CANVAS {
+	BOUND bound;
+	int * buffer;
+} CANVAS;
 
-void destroy_children(QNODE * q);
-
-void check_qnode(QNODE * q, BOUND b);
-
-void subdivide(QNODE * q, BOUND b);
-
-int search_qtree(QNODE * q, int x, int y);
-
-/********** END QNODE *************/
-
-/***************** WINDOW **************/
-
-typedef struct _WINDOW {
+typedef struct _VGA_WINDOW {
 	int id;
+	FRAME frame;
+	CANVAS canvas;
+    int color;
 	QNODE * root;
-	struct _WINDOW * next;
-} WINDOW;
-
-WINDOW * WINDOW_LIST;
-
-WINDOW * create_window(int x, int y, int width, int height);
-
-/************** END WINDOW *************/
-
-/*************** TREE MANAGEMENT *************/
-
-BOUND get_intersection(BOUND * a, BOUND * b);
-
-void build_quadtrees();
-
-/*************** END TREE MANAGEMENT *********/
+	struct _VGA_WINDOW * next;
+    struct _VGA_WINDOW * prev;
+} VGA_WINDOW;
 
 #endif /* WINDOW_H */
